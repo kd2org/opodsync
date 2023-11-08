e81aa57bac72fa6cad72ecea4f052fc33c8b331b
Lars Albrecht <lars.albrecht@gmail.com>
2023-11-08 22:03:00 +0100

    Add url encode function Clean up code

--- server/inc/GPodder.php
+++ server/inc/GPodder.php
@@ -2,101 +2,104 @@
 
 class GPodder
 {
-	protected DB $db;
-	public ?\stdClass $user = null;
+    protected DB $db;
+    public ?\stdClass $user = null;
 
-	public function __construct(DB $db)
-	{
-		$this->db = $db;
+    public function __construct(DB $db)
+    {
+        $this->db = $db;
 
-		if (isset($_COOKIE[session_name()]) || !empty($_POST['login'])) {
-			if (isset($_GET['token']) && ctype_alnum($_GET['token'])) {
-				session_id($_GET['token']);
-			}
+        if (!empty($_POST['login']) || isset($_COOKIE[session_name()])) {
+            if (isset($_GET['token']) && ctype_alnum($_GET['token'])) {
+                session_id($_GET['token']);
+            }
 
-			@session_start();
+            @session_start();
 
-			if (!empty($_SESSION['user'])) {
-				$this->user = $_SESSION['user'];
-			}
-		}
-	}
+            if (!empty($_SESSION['user'])) {
+                $this->user = $_SESSION['user'];
+            }
+        }
+    }
 
-	public function auth(): ?string {
-		if (empty($_POST['login']) || empty($_POST['password'])) {
-			return null;
-		}
+    public function auth(): ?string {
+        if (empty($_POST['login']) || empty($_POST['password'])) {
+            return null;
+        }
 
-		$user = $this->db->firstRow('SELECT * FROM users WHERE name = ?;', trim($_POST['login']));
+        $user = $this->db->firstRow('SELECT * FROM users WHERE name = ?;', trim($_POST['login']));
 
-		if (!password_verify(trim($_POST['password']), $user->password ?? '')) {
-			return 'Invalid username/password';
-		}
+        if (!$user || !password_verify(trim($_POST['password']), $user->password ?? '')) {
+            return 'Invalid username/password';
+        }
 
-		$_SESSION['user'] = $this->user = $user;
+        $_SESSION['user'] = $this->user = $user;
 
-		if (!empty($_GET['token'])) {
-			$_SESSION['app_password'] = sprintf('%s:%s', $_GET['token'], sha1($user->password . $_GET['token']));
-		}
+        if (!empty($_GET['token'])) {
+            $_SESSION['app_password'] = sprintf('%s:%s', $_GET['token'], sha1($user->password . $_GET['token']));
+        }
 
-		return null;
-	}
+        return null;
+    }
 
 
-	public function canSubscribe(): bool {
-		if (ENABLE_SUBSCRIPTIONS) {
-			return true;
-		}
+    public function canSubscribe(): bool {
+        if (ENABLE_SUBSCRIPTIONS) {
+            return true;
+        }
 
-		if (!$this->db->firstColumn('SELECT COUNT(*) FROM users;')) {
-			return true;
-		}
+        if (!$this->db->firstColumn('SELECT COUNT(*) FROM users;')) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	public function subscribe(string $name, string $password): ?string {
-		if (trim($name) === '' || !preg_match('/^\w[\w\d_-]+$/', $name)) {
-			return 'Invalid username. Allowed is: \w[\w\d_-]+';
-		}
+    public function subscribe(string $name, string $password): ?string {
+        if (trim($name) === '' || !preg_match('/^\w[\w_-]+$/', $name)) {
+            return 'Invalid username. Allowed is: \w[\w\d_-]+';
+        }
 
-		if ($name == 'current') {
-			return 'This username is locked, please choose another one.';
-		}
+        if ($name === 'current') {
+            return 'This username is locked, please choose another one.';
+        }
 
-		$password = trim($password);
+        $password = trim($password);
 
-		if (strlen($password) < 8) {
-			return 'Password is too short';
-		}
+        if (strlen($password) < 8) {
+            return 'Password is too short';
+        }
 
-		if ($this->db->firstColumn('SELECT 1 FROM users WHERE name = ?;', $name)) {
-			return 'Username already exists';
-		}
+        if ($this->db->firstColumn('SELECT 1 FROM users WHERE name = ?;', $name)) {
+            return 'Username already exists';
+        }
 
-		$this->db->simple('INSERT INTO users (name, password) VALUES (?, ?);', trim($name), password_hash($password, null));
-		return null;
-	}
+        $this->db->simple('INSERT INTO users (name, password) VALUES (?, ?);', trim($name), password_hash($password, null));
+        return null;
+    }
 
-	public function generateCaptcha(): string
-	{
-		$n = '';
-		$c = '';
+    /**
+     * @throws Exception
+     */
+    public function generateCaptcha(): string
+    {
+        $n = '';
+        $c = '';
 
-		for ($i = 0; $i < 4; $i++) {
-			$j = random_int(0, 9);
-			$c .= $j;
-			$n .= sprintf('<b>%d</b><i>%d</i>', random_int(0, 9), $j);
-		}
+        for ($i = 0; $i < 4; $i++) {
+            $j = random_int(0, 9);
+            $c .= $j;
+            $n .= sprintf('<b>%d</b><i>%d</i>', random_int(0, 9), $j);
+        }
 
-		$n .= sprintf('<input type="hidden" name="cc" value="%s" />', sha1($c . __DIR__));
+        $n .= sprintf('<input type="hidden" name="cc" value="%s" />', sha1($c . __DIR__));
 
-		return $n;
-	}
+        return $n;
+    }
 
-	public function checkCaptcha(string $captcha, string $check): bool
-	{
-		$captcha = trim($captcha);
-		return sha1($captcha . __DIR__) == $check;
-	}
+    public function checkCaptcha(string $captcha, string $check): bool
+    {
+        $captcha = trim($captcha);
+        return sha1($captcha . __DIR__) === $check;
+    }
 }
