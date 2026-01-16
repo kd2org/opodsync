31dd68c43777d6691ade13564cca86422e9fd531
bohwaz <>
2026-01-16 14:04:58 +0000

    Implement ability to add new subscriptions from web UI, thanks @nebulade
    FossilOrigin-Name: 67b9c1d7ea0ac1c6f92d175a767dee58d5e7c186c2aaa0d44e5527a79e57706a

--- server/subscriptions.php
+++ server/subscriptions.php
@@ -9,7 +9,25 @@ if (!$gpodder->user) {
 	exit;
 }
 
+$error = null;
+$success = null;
+
+// Handle new subscription
+if (!empty($_POST['feed_url'])) {
+	$error = $gpodder->addSubscription($_POST['feed_url']);
+	if (!$error) {
+		$success = 'Successfully subscribed to the feed!';
+	}
+}
+
+// Handle unsubscribe
+if (!empty($_POST['unsubscribe']) && is_numeric($_POST['unsubscribe'])) {
+	if ($gpodder->removeSubscription((int)$_POST['unsubscribe'])) {
+		$success = 'Successfully unsubscribed from the feed.';
+	}
+}
+
 $subscriptions = $gpodder->listActiveSubscriptions();
 
-$tpl->assign(compact('subscriptions'));
+$tpl->assign(compact('subscriptions', 'error', 'success'));
 $tpl->display('subscriptions.tpl');
