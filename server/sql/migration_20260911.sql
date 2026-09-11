125b3a6cfb08bd7a07ea4a3e103ca8076f68f59d
bohwaz <>
2026-09-11 22:56:10 +0000

    Add new indexes
    FossilOrigin-Name: 7adef01eeb92d6c64020ff2a57de4387101f2fc9f18a3d6e65f19e8fb3ad80a1

--- server/sql/migration_20260911.sql
+++ server/sql/migration_20260911.sql
@@ -1,2 +1,5 @@
 CREATE INDEX episodes_media_url ON episodes (media_url);
 CREATE INDEX episodes_actions_url ON episodes_actions (url);
+
+CREATE INDEX episodes_actions_subscription_action ON episodes_actions (subscription, action);
+CREATE INDEX episodes_feed_pubdate ON episodes (feed, pubdate DESC);
